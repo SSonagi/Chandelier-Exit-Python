a5d8cfc84ca11a1512fc0229d69748c41dfc010b
William Lee <52926429+SSonagi@users.noreply.github.com>
2023-05-26 14:02:33 -0400

    Add exit when profit > 3%

--- chandelier.py
+++ chandelier.py
@@ -7,9 +7,9 @@ import datetime
 
 # 거래소 객체를 생성합니다.
 exchange = ccxt.kucoin({
-    'apiKey': '',
-    'secret': '',
-    'password': '',
+    'apiKey': '644d9a42a5cf6e00016e2518',
+    'secret': '8152fa44-eb62-4e3d-8fd7-e64d597317a1',
+    'password': 'Sangwook12',
     'enableRateLimit': True,
     'rateLimit': 1000,
     'options': {
@@ -32,11 +32,12 @@ shortStopPrev = 0.0
 dirPrev = -1  # 올라갈떄는 1 내려갈떄는 -1
 closePrev = -1
 
-since = int(time.time() * 1000) - 86400000 - (60000 * period * 15)
-while since < int(time.time() * 1000) - (60000 * period * 15):
+while True:
     try:
+
+
         # Fetch the OHLCV data
-        ohlcv = exchange.fetch_ohlcv(symbol, timeframe, since=since, limit=period) 
+        ohlcv = exchange.fetch_ohlcv(symbol, timeframe, limit=period) 
         
         # 히스토리 데이터에서 각 값에 해당하는 배열을 추출합니다.
         high = np.array([ohlcv[i][2] for i in range(period)])
@@ -50,10 +51,7 @@ while since < int(time.time() * 1000) - (60000 * period * 15):
         lowest_price = min(closes)
 
         # chandelier exit strategy 계산을 위한 ATR값을 구합니다.
-        atr = ATR(high, low, closes, timeperiod=period-1)[-1]
-
-        # ticker = exchange.fetch_ticker(symbol)
-        # close = ticker['last']   
+        atr = ATR(high, low, closes, timeperiod=period-1)[-1]  
             
         longStop = highest_price - (multiplier * atr)
 
@@ -88,7 +86,7 @@ while since < int(time.time() * 1000) - (60000 * period * 15):
         if sellSignal: print("selling!")
 
         if buySignal or sellSignal:
-            print(datetime.datetime.fromtimestamp((since + (60000 * 15))/1000))
+            print(datetime.datetime.now())
 
             print("current: " + str(closes[-1]))
             print("high: " + str(highest_price) 
@@ -108,27 +106,28 @@ while since < int(time.time() * 1000) - (60000 * period * 15):
         # 현재 잔고를 업데이트합니다.
         balance = exchange.fetch_balance()['USDT']['free']
 
-        # if buySignal and balance >= 1:
-        #     amount = balance / close
-        #     exchange.create_market_buy_order(symbol, amount)
-        #     buy_price = close
-        #     print(f"Buy: {buy_price:.2f}")
-        #     btc_balance = exchange.fetch_balance()['BTC']['free']
+        ticker = exchange.fetch_ticker(symbol)
+        close = ticker['last'] 
 
-        #     # 현재 포지션과 잔고를 출력합니다.
-        #     print('USDT:', balance)
-        #     print(f"현재 보유중인 BTC 수량: {btc_balance:.8f}")
-        # elif sellSignal and balance < 1:
-        #     btc_balance = exchange.fetch_balance()['BTC']['free']
-        #     exchange.create_market_sell_order(symbol, btc_balance)
-        #     sell_price = close
-        #     print(f"Sell: {sell_price:.2f}")
-        #     profit = (sell_price - buy_price) / buy_price * 100
-        #     print(f"Profit/loss: {profit:.2f}%")
+        if buySignal and balance >= 1:
+            amount = balance / close
+            exchange.create_market_buy_order(symbol, amount)
+            buy_price = close
+            print(f"Buy: {buy_price:.2f}")
+            btc_balance = exchange.fetch_balance()['BTC']['free']
 
-        # time.sleep(60)
+            # 현재 포지션과 잔고를 출력합니다.
+            print('USDT:', balance)
+            print(f"현재 보유중인 BTC 수량: {btc_balance:.8f}")
+        elif (sellSignal or close > buy_price + (buy_price * 0.03)) and balance < 1:
+            btc_balance = exchange.fetch_balance()['BTC']['free']
+            exchange.create_market_sell_order(symbol, btc_balance)
+            sell_price = close
+            print(f"Sell: {sell_price:.2f}")
+            profit = (sell_price - buy_price) / buy_price * 100
+            print(f"Profit/loss: {profit:.2f}%")
 
-        since += (60000 * 15)
+        time.sleep(60 * 15)
     except Exception as e:
         print(e)
 
